bec5460eddfb4b12881993132119522534251986
Eduarda Mesquita <126674559+eduardammag@users.noreply.github.com>
2024-11-19 14:43:28 -0300

    Update questao5.cpp

--- Lista02.cpp/questao5.cpp
+++ Lista02.cpp/questao5.cpp
@@ -3,100 +3,196 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <limits>
-
 using namespace std;
 
-// Representação de uma aresta
-struct Edge {
-    int u, v, weight;
-    bool inMST;
+typedef int vertex;
+
+// Classe para representar um nó da lista de adjacência
+class EdgeNode {
+private:
+    vertex m_otherVertex;
+    EdgeNode* m_next;
+
+public:
+    EdgeNode(vertex otherVertex, EdgeNode* next)
+        : m_otherVertex(otherVertex), m_next(next) {}
+
+    vertex otherVertex() const { return m_otherVertex; }
+    EdgeNode* next() const { return m_next; }
+    void setNext(EdgeNode* next) { m_next = next; }
 };
 
-// União-Find para gerenciar componentes conectados
-struct UnionFind {
-    vector<int> parent, rank;
-    UnionFind(int n) : parent(n), rank(n, 0) {
-        for (int i = 0; i < n; ++i) parent[i] = i;
-    }
-    int find(int x) {
-        if (x != parent[x]) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-    bool unite(int x, int y) {
-        int rootX = find(x), rootY = find(y);
-        if (rootX == rootY) return false;
-        if (rank[rootX] < rank[rootY]) swap(rootX, rootY);
-        parent[rootY] = rootX;
-        if (rank[rootX] == rank[rootY]) rank[rootX]++;
-        return true;
-    }
-};
+// Classe que representa o grafo usando listas de adjacência
+class GraphAdjList {
+private:
+    int m_numVertices;
+    int m_numEdges;
+    EdgeNode** m_edges; // Lista de adjacências
+    vector<tuple<vertex, vertex, int>> m_allEdges; // Lista de todas as arestas (v1, v2, peso)
 
-// Função para calcular a MST usando Kruskal
-pair<int, vector<Edge>> kruskal(int n, vector<Edge>& edges) {
-    sort(edges.begin(), edges.end(), [](Edge& a, Edge& b) {
-        return a.weight < b.weight;
-    });
-    UnionFind uf(n);
-    vector<Edge> mst;
-    int totalWeight = 0;
-    for (Edge& e : edges) {
-        if (uf.unite(e.u, e.v)) {
-            mst.push_back(e);
-            totalWeight += e.weight;
-            e.inMST = true;
+public:
+    GraphAdjList(int numVertices)
+        : m_numVertices(numVertices), m_numEdges(0) {
+        m_edges = new EdgeNode*[numVertices];
+        for (vertex i = 0; i < numVertices; i++) {
+            m_edges[i] = nullptr;
         }
     }
-    return {totalWeight, mst};
+
+    // Adiciona uma aresta ao grafo
+    void addEdge(vertex v1, vertex v2, int weight) {
+        m_edges[v1] = new EdgeNode(v2, m_edges[v1]);
+        m_edges[v2] = new EdgeNode(v1, m_edges[v2]); // Grafo não-direcionado
+        m_allEdges.emplace_back(v1, v2, weight);
+        m_numEdges++;
+    }
+
+    // Retorna o número de vértices
+    int numVertices() const {
+        return m_numVertices;
+    }
+
+    // Retorna todas as arestas
+    vector<tuple<vertex, vertex, int>> edges() const {
+        return m_allEdges;
+    }
+
+    // Imprime o grafo
+    void print() const {
+        for (vertex i = 0; i < m_numVertices; i++) {
+            EdgeNode* edge = m_edges[i];
+            cout << "Vértice " << i << ": ";
+            while (edge) {
+                cout << edge->otherVertex() << " ";
+                edge = edge->next();
+            }
+            cout << endl;
+        }
+    }
+
+    ~GraphAdjList() {
+        for (vertex i = 0; i < m_numVertices; i++) {
+            EdgeNode* edge = m_edges[i];
+            while (edge) {
+                EdgeNode* next = edge->next();
+                delete edge;
+                edge = next;
+            }
+        }
+        delete[] m_edges;
+    }
+};
+
+// Classe auxiliar para a Union-Find (Disjoint-Set)
+class UnionFind {
+private:
+    vector<int> parent, rank;
+
+public:
+    UnionFind(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+        for (int i = 0; i < size; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int u) {
+        if (u != parent[u])
+            parent[u] = find(parent[u]);
+        return parent[u];
+    }
+
+    void unionSets(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
+            }
+        }
+    }
+};
+
+// Algoritmo de Kruskal para encontrar a MST
+int kruskal(GraphAdjList& graph, vector<tuple<vertex, vertex, int>>& mstEdges) {
+    vector<tuple<vertex, vertex, int>> edges = graph.edges();
+    sort(edges.begin(), edges.end(), [](auto& e1, auto& e2) {
+        return get<2>(e1) < get<2>(e2);
+    });
+
+    UnionFind uf(graph.numVertices());
+    int mstCost = 0;
+
+    for (auto& [v1, v2, weight] : edges) {
+        if (uf.find(v1) != uf.find(v2)) {
+            uf.unionSets(v1, v2);
+            mstCost += weight;
+            mstEdges.push_back({v1, v2, weight});
+        }
+    }
+
+    return mstCost;
 }
 
-// Função principal para encontrar a aresta crítica
-Edge findCriticalEdge(int n, vector<Edge>& edges) {
-    auto [mstWeight, mst] = kruskal(n, edges);
-    Edge criticalEdge = {-1, -1, -1, false};
+// Calcula o aumento crítico no custo ao remover uma aresta da MST
+int criticalEdgeMST(GraphAdjList& graph) {
+    vector<tuple<vertex, vertex, int>> mstEdges;
+    int mstCost = kruskal(graph, mstEdges);
+
     int maxIncrease = 0;
 
-    for (Edge& e : mst) {
-        UnionFind uf(n);
-        int newWeight = 0;
+    for (auto& edge : mstEdges) {
+        GraphAdjList tempGraph(graph.numVertices());
 
-        for (Edge& other : edges) {
-            if (other.inMST && other.u != e.u && other.v != e.v) {
-                if (uf.unite(other.u, other.v)) {
-                    newWeight += other.weight;
-                }
+        // Reconstroi o grafo sem a aresta removida
+        for (auto& e : graph.edges()) {
+            if (e != edge) {
+                tempGraph.addEdge(get<0>(e), get<1>(e), get<2>(e));
             }
         }
 
-        for (Edge& other : edges) {
-            if (!other.inMST && uf.find(other.u) != uf.find(other.v)) {
-                newWeight += other.weight;
-                break;
-            }
-        }
+        vector<tuple<vertex, vertex, int>> newMSTEdges;
+        int newMSTCost = kruskal(tempGraph, newMSTEdges);
 
-        int increase = newWeight - mstWeight;
-        if (increase > maxIncrease) {
-            maxIncrease = increase;
-            criticalEdge = e;
-        }
+        maxIncrease = max(maxIncrease, newMSTCost - mstCost);
     }
 
-    return criticalEdge;
+    return maxIncrease;
 }
 
 int main() {
-    int n = 5; // Número de vértices
-    vector<Edge> edges = {
-        {0, 1, 1, false}, {0, 2, 3, false}, {1, 2, 2, false},
-        {1, 3, 4, false}, {2, 4, 5, false}, {3, 4, 6, false}
-    };
+    GraphAdjList graph(6);
+    graph.addEdge(0, 1, 4);
+    graph.addEdge(0, 2, 4);
+    graph.addEdge(1, 2, 2);
+    graph.addEdge(1, 0, 4);
+    graph.addEdge(2, 3, 3);
+    graph.addEdge(2, 5, 2);
+    graph.addEdge(2, 4, 4);
+    graph.addEdge(3, 4, 3);
+    graph.addEdge(5, 4, 3);
 
-    Edge critical = findCriticalEdge(n, edges);
-    cout << "Aresta crítica: (" << critical.u << ", " 
-         << critical.v << ") com peso " << critical.weight << endl;
+    cout << "Grafo (Lista de Adjacência):" << endl;
+    graph.print();
+
+    vector<tuple<vertex, vertex, int>> mstEdges;
+    int mstCost = kruskal(graph, mstEdges);
+
+    cout << "\nÁrvore Geradora Mínima (MST):" << endl;
+    for (auto& [v1, v2, weight] : mstEdges) {
+        cout << "(" << v1 << ", " << v2 << ", " << weight << ")" << endl;
+    }
+    cout << "Custo Total da MST: " << mstCost << endl;
+
+    int criticalIncrease = criticalEdgeMST(graph);
+    cout << "\nAumento Máximo ao Remover uma Aresta Crítica: " << criticalIncrease << endl;
 
     return 0;
 }
